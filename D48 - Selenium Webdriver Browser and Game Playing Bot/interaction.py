971f8d03ee06866abb469d0998732ba08692ca12
ajjoyib <hamidullo287@gmail.com>
2024-01-15 04:41:29 +0900

    D48 - Automate filling out forms and clicking buttons with Selenium

--- D48 - Selenium Webdriver Browser and Game Playing Bot/interaction.py
+++ D48 - Selenium Webdriver Browser and Game Playing Bot/interaction.py
@@ -1,14 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=chrome_options)
-url = "https://en.wikipedia.org/wiki/Main_Page" 
+url = "https://www.linkedin.com/login"
+email = "hamidullo287@gmail.com"
+password = "20@lfraganuS04"
+
+driver = webdriver.Chrome(chrome_options)
 driver.get(url)
 
-num_of_articles = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
-print(num_of_articles.text)
+email_input = driver.find_element(By.ID, "username")
+password_input = driver.find_element(By.ID, "password")
 
-driver.quit()
+# email_input.send_keys(email, Keys.TAB)
+email_input.send_keys(email)
+password_input.send_keys(password, Keys.ENTER)
+
+
+
--- D48 - Selenium Webdriver Browser and Game Playing Bot/qiyqim.py
+++ D48 - Selenium Webdriver Browser and Game Playing Bot/qiyqim.py
@@ -38,3 +38,36 @@ driver.get(url)
 print(driver.find_element(By.XPATH, '//*[@id="corePrice_feature_div"]/div/div/span[1]/span[2]').text)
 
 driver.quit()
+
+
+
+# interaction.py
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+url = "https://en.wikipedia.org/wiki/Main_Page" 
+driver.get(url)
+
+num_of_articles = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
+print(num_of_articles.text)
+
+# Clicking the link
+# num_of_articles.click()
+
+# Finding element by Link Text
+all_portals = driver.find_element(By.LINK_TEXT, "Content portals")
+# all_portals.click()
+
+# Find the "Search" <input> by Name
+search = driver.find_element(By.NAME, "search")
+
+# Sending keyboard input
+# search.send_keys("Python") # Can send only text
+search.send_keys("Python", Keys.ENTER)
+
+# driver.quit()
