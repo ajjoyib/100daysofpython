3d3b862304a7a68c48920903da4db7b368908e90
ajjoyib <hamidullo287@gmail.com>
2024-01-23 05:45:57 +0900

    D51 - Internet Speed Twitter Complaint Bot - Part I Finished

--- keys.py
+++ keys.py
@@ -1,12 +1,3 @@
-import smtplib
-
-MY_EMAIL = "hamidullo287@gmail.com"
-
-with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-        connection.starttls()
-        connection.login("MY_EMAIL", "20@lfraganuS04")
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs="h.davlatov@outlook.com",
-            msg=f"Subject:Happy Birthday"
-        )
+# keys.py 
+EMAIL = "hamidullo287@gmail.com"
+PASSWORD = "twitt3R_@uth"
