be501d5b5600df6c5800e6ad434c5eead49d2527
ajjoyib <hamidullo287@gmail.com>
2024-01-28 09:31:43 +0900

    Logout

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -69,3 +69,8 @@ def load_logged_in_user():
         g.user = get_db().execute(
             "SELECT * FROM user WHERE id = ?", (user_id,)
         ).fetchone()
+
+@bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
