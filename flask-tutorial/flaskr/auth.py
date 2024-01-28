87e36b7ae722b56adc2fb44e499e93b5357d199f
ajjoyib <hamidullo287@gmail.com>
2024-01-28 09:38:19 +0900

    Require authentication in other views

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -5,6 +5,8 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 from db import get_db
 
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
 @bp.route("/register", methods=("GET", "POST"))
 def register():
     if request.method == "POST":
@@ -74,3 +76,14 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for("index"))
+
+# require authentication in other views
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
