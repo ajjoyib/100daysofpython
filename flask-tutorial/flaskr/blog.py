5bad5b49a65f3885af15c278a2313b4a171b1e74
ajjoyib <hamidullo287@gmail.com>
2024-01-31 17:25:07 +0900

    D56. Finished!

--- flask-tutorial/flaskr/blog.py
+++ flask-tutorial/flaskr/blog.py
@@ -15,7 +15,7 @@ def index():
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+    return render_template('blog/hamidullo.html', posts=posts)
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
