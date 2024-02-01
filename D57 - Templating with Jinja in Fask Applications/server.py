35ec6bbb149be105a9b5efb27cfda5a22fe0c7e4
ajjoyib <hamidullo287@gmail.com>
2024-02-01 16:23:05 +0900

    D57. 442. Multiline Statements with Jinja

--- D57 - Templating with Jinja in Fask Applications/server.py
+++ D57 - Templating with Jinja in Fask Applications/server.py
@@ -27,5 +27,13 @@ def guess(name):
     return render_template("guess.html", person_name=name, person_gender=gender, person_age=person_age)
 
 
+@app.route("/blog")
+def blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
