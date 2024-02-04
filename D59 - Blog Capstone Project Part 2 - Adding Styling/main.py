5e84bbcad3a104ae542fbdd04568270e9bf40db0
ajjoyib <hamidullo287@gmail.com>
2024-02-04 20:01:36 +0900

    D59. 455 - Make the About and Contact Pages Work Done!

--- D59 - Blog Capstone Project Part 2 - Adding Styling/main.py
+++ D59 - Blog Capstone Project Part 2 - Adding Styling/main.py
@@ -11,5 +11,29 @@ def index():
     return render_template("index.html", css_url=css_url, js_url=js_url, favicon_url=favicon_url)
 
 
+@app.route('/about')
+def about():
+    css_url = url_for('static', filename='/css/styles.css')
+    js_url = url_for('static', filename='/js/scripts.js')
+    favicon_url = url_for('static', filename='/assets/favicon.ico')
+    return render_template("about.html", css_url=css_url, js_url=js_url, favicon_url=favicon_url)
+
+
+@app.route('/contact')
+def contact():
+    css_url = url_for('static', filename='/css/styles.css')
+    js_url = url_for('static', filename='/js/scripts.js')
+    favicon_url = url_for('static', filename='/assets/favicon.ico')
+    return render_template("contact.html", css_url=css_url, js_url=js_url, favicon_url=favicon_url)
+
+
+@app.route('/post')
+def post():
+    css_url = url_for('static', filename='/css/styles.css')
+    js_url = url_for('static', filename='/js/scripts.js')
+    favicon_url = url_for('static', filename='/assets/favicon.ico')
+    return render_template("post.html", css_url=css_url, js_url=js_url, favicon_url=favicon_url)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
