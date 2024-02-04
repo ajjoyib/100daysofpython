f9af9ee49dd2824a58ad2f6eee1cef54b941b4a5
ajjoyib <hamidullo287@gmail.com>
2024-02-04 20:52:28 +0900

    D59. 456 - Fetch and render the blog posts from an API. Done!

--- D59 - Blog Capstone Project Part 2 - Adding Styling/main.py
+++ D59 - Blog Capstone Project Part 2 - Adding Styling/main.py
@@ -1,14 +1,20 @@
 # start a flask app that opens index.html when run on a localhost
 from flask import *
+from datetime import datetime, timedelta
+from random import randint
+
+import requests
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    response = requests.get(npoint_url)
+    data = response.json()
     css_url = url_for('static', filename='/css/styles.css')
     js_url = url_for('static', filename='/js/scripts.js')
     favicon_url = url_for('static', filename='/assets/favicon.ico')
-    return render_template("index.html", css_url=css_url, js_url=js_url, favicon_url=favicon_url)
+    return render_template("index.html", css_url=css_url, js_url=js_url, favicon_url=favicon_url, posts=data, datetime=datetime, randint=randint)
 
 
 @app.route('/about')
@@ -33,6 +39,9 @@ def post():
     js_url = url_for('static', filename='/js/scripts.js')
     favicon_url = url_for('static', filename='/assets/favicon.ico')
     return render_template("post.html", css_url=css_url, js_url=js_url, favicon_url=favicon_url)
+
+npoint_url = "https://api.npoint.io/674f5423f73deab1e9a7"
+
 
 
 if __name__ == "__main__":
