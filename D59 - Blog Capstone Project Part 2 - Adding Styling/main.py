e6ca0273fa8bb29b7f0b9de31537af9425ceaee9
ajjoyib <hamidullo287@gmail.com>
2024-02-04 21:36:39 +0900

    D59. Finished!!! - Need to review a few times.

--- D59 - Blog Capstone Project Part 2 - Adding Styling/main.py
+++ D59 - Blog Capstone Project Part 2 - Adding Styling/main.py
@@ -1,64 +1,35 @@
-from flask import Flask, render_template, url_for
-from datetime import datetime, timedelta
-from random import randint
+from flask import Flask, render_template
 import requests
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
 
-npoint_url = "https://api.npoint.io/674f5423f73deab1e9a7"
-
 
 @app.route('/')
-def index():
-    response = requests.get(npoint_url)
-    data = response.json()
-
-    css_url = url_for('static', filename='/css/styles.css')
-    js_url = url_for('static', filename='/js/scripts.js')
-    favicon_url = url_for('static', filename='/assets/favicon.ico')
-
-    return render_template("index.html", css_url=css_url, js_url=js_url, favicon_url=favicon_url, posts=data,
-                           datetime=datetime, randint=randint)
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    css_url = url_for('static', filename='/css/styles.css')
-    js_url = url_for('static', filename='/js/scripts.js')
-    favicon_url = url_for('static', filename='/assets/favicon.ico')
-
-    return render_template("about.html", css_url=css_url, js_url=js_url, favicon_url=favicon_url)
+    return render_template("about.html")
 
 
-@app.route('/contact')
+@app.route("/contact")
 def contact():
-    css_url = url_for('static', filename='/css/styles.css')
-    js_url = url_for('static', filename='/js/scripts.js')
-    favicon_url = url_for('static', filename='/assets/favicon.ico')
-
-    return render_template("contact.html", css_url=css_url, js_url=js_url, favicon_url=favicon_url)
+    return render_template("contact.html")
 
 
-@app.route('/post/<int:post_id>')
-def post(post_id):
-    response = requests.get(npoint_url)
-    data = response.json()
-
+@app.route("/post/<int:index>")
+def show_post(index):
     requested_post = None
-
-    for blog_post in data:
-        if blog_post["id"] == post_id:
+    for blog_post in posts:
+        if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
-    css_url = url_for('static', filename='/css/styles.css')
-    js_url = url_for('static', filename='/js/scripts.js')
-    favicon_url = url_for('static', filename='/assets/favicon.ico')
-
-    return render_template("post.html", css_url=css_url, js_url=js_url, favicon_url=favicon_url, post=selected_post,
-                           datetime=datetime, randint=randint)
-
-
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
