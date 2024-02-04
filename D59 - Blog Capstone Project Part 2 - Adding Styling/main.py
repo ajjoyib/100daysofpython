f6097b6150488e77688d3550cf060ad116528f6d
ajjoyib <hamidullo287@gmail.com>
2024-02-04 19:07:48 +0900

    D59. 453 - Fix the header and footer. Done!

---
+++ D59 - Blog Capstone Project Part 2 - Adding Styling/main.py
@@ -0,0 +1,15 @@
+# start a flask app that opens index.html when run on a localhost
+from flask import *
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    css_url = url_for('static', filename='/css/styles.css')
+    js_url = url_for('static', filename='/js/scripts.js')
+    favicon_url = url_for('static', filename='/assets/favicon.ico')
+    return render_template("index.html", css_url=css_url, js_url=js_url, favicon_url=favicon_url)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
