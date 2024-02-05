cffffaf759a81d038311afa82fd3175dd57806ef
ajjoyib <hamidullo287@gmail.com>
2024-02-05 22:59:43 +0900

    D60. 460 - Handle POST Requests with Flask Servers. Done!

--- D60 - Make POST Requests with Flask and HTML Forms/main.py
+++ D60 - Make POST Requests with Flask and HTML Forms/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=["POST"])
+def receive_data():
+    if request.method == "POST":
+        name = request.form["username"]
+        password = request.form["password"]
+
+        return render_template("login.html", name=name, password=password)
+    return "Oops! Something went wrong!"
 
 
 if __name__ == "__main__":
