71dc667b4bd3f842f8cfaa02c4606b8e073f66ec
ajjoyib <hamidullo287@gmail.com>
2024-03-01 22:02:56 +0900

    487. Requirement 2 - Be able to edit a movie's rating and review. Done!

--- D64 - My Top 10 Movies Website/main.py
+++ D64 - My Top 10 Movies Website/main.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 import requests
 
 
@@ -38,34 +38,10 @@ with app.app_context():
     db.create_all()
 
 
-# # After adding the new_movie, the code needs to be commented out/deleted.
-# # So you are not trying to add the same movie twice. The db will reject non-unique movie titles.
-# new_movie = Movie(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's "
-#                 "sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to "
-#                 "a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favorite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-
-# second_movie = Movie(
-#     title="Avatar The Way of Water",
-#     year=2022,
-#     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-#     rating=7.3,
-#     ranking=9,
-#     review="I liked the water.",
-#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-# )
-#
-# with app.app_context():
-#     db.session.add(new_movie)
-#     db.session.add(second_movie)
-#     db.session.commit()
+class RateMovieForm(FlaskForm):
+    rating = StringField(label='Your Rating out of 10 e.g. 7.5', validators=[DataRequired(), ])
+    review = StringField(label='Your Review', validators=[DataRequired(), Length(min=1, max=75)])
+    submit = SubmitField(label='Done')
 
 
 @app.route("/")
@@ -73,6 +49,19 @@ def home():
     result = db.session.execute(db.select(Movie))
     all_movies = result.scalars()
     return render_template("index.html", movies=all_movies)
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def rate_movie():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("edit.html", movie=movie, form=form)
 
 
 if __name__ == '__main__':
