e0952fabc818957ca33784fcb12f15de2aaff748
ajjoyib <hamidullo287@gmail.com>
2024-03-02 09:46:47 +0900

    Incorporate themoviedb.org API

--- D64 - My Top 10 Movies Website/keys.py
+++ D64 - My Top 10 Movies Website/keys.py
@@ -1,5 +1,4 @@
 MOVIE_DB_API_KEY = "6e59753c154de03e426cf6b18175566d"
-MOVIE_DB_SEARCH_URL = f"https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key={MOVIE_DB_API_KEY}"
-# MOVIE_DB_IMAGE_URL = "https://api.themoviedb.org/3/movie/550"
-MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/original/"
-MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie/343611?api_key=API_KEY"
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
